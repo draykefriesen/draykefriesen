8a1c2977839e1155b6ff04cb0729dcdb3a2199a8
drayke friesen <draykefriesen@gmail.com>
2020-10-23 13:00:04 -0500

    added undraw image for new pricing / info section

--- src/components/info-section/InfoSection.js
+++ src/components/info-section/InfoSection.js
@@ -1,23 +1,26 @@
 import React from "react"
 import TextSection from "./text-section/TextSection"
 import GraphicCard from "./graphic-card/GraphicCard"
-import {BackgroundGradientTop, BackgroundGradientBottom} from '../background-gradient/BackgroundGradient'
-import {BackgroundStripes2} from '../background-stripes/BackgroundStripes'
+import {
+  BackgroundGradientTop,
+  BackgroundGradientBottom,
+} from "../background-gradient/BackgroundGradient"
+import { BackgroundStripes2 } from "../background-stripes/BackgroundStripes"
 import styles from "./infoSection.module.scss"
 
 const InfoSection = ({ scroll }) => {
   return (
     <div className={styles.infoWrapper}>
-    <BackgroundGradientTop section='info-section' />
+      <BackgroundGradientTop section="info-section" />
       <TextSection scroll={scroll} />
       <div className={styles.graphicCardWrapper}>
-      <GraphicCard type={"web-dev"} />
-      <GraphicCard type={"web-design"} />
+        <GraphicCard type="web-dev" cardNumber="cardOne" />
+        <GraphicCard type="mobile" cardNumber="cardTwo" />
+        <GraphicCard type="web-design" cardNumber="cardThree" />
       </div>
       <BackgroundStripes2 />
-      <BackgroundGradientBottom section='info-section'/>
+      <BackgroundGradientBottom section="info-section" />
     </div>
-
   )
 }
 
--- src/components/info-section/graphic-card/GraphicCard.js
+++ src/components/info-section/graphic-card/GraphicCard.js
@@ -1,22 +1,41 @@
 import React from "react"
 import WebDevIcon from "../../../media/svg/web-dev-card.svg"
+import MobileIcon from "../../../media/svg/mobile-card.svg"
 import WebDesignIcon from "../../../media/svg/web-design-card.svg"
 import graphicCardStyles from "./graphicCard.module.scss"
 import { IoIosCheckmarkCircleOutline } from "react-icons/io"
 
-const GraphicCard = ({ type }) => {
+const GraphicCard = ({ type, cardNumber }) => {
   return (
     <div
-      className={`${graphicCardStyles.cardWrapper} ${
-        type === "web-dev"
-          ? graphicCardStyles.cardOne
-          : graphicCardStyles.cardTwo
-      }`}
+      className={`${graphicCardStyles.cardWrapper} ${graphicCardStyles[cardNumber]}`}
     >
       <div className={graphicCardStyles.card}>
         <div className={graphicCardStyles.borderTop}></div>
         <ul className={graphicCardStyles.textList}>
           {type === "web-dev" && (
+            <>
+              <li className={graphicCardStyles.listItem}>
+                <IoIosCheckmarkCircleOutline
+                  className={graphicCardStyles.icon}
+                />
+                <p>Blogs</p>
+              </li>
+              <li className={graphicCardStyles.listItem}>
+                <IoIosCheckmarkCircleOutline
+                  className={graphicCardStyles.icon}
+                />
+                <p>Portfolios</p>
+              </li>
+              <li className={graphicCardStyles.listItem}>
+                <IoIosCheckmarkCircleOutline
+                  className={graphicCardStyles.icon}
+                />
+                <p>E-commerce</p>
+              </li>
+            </>
+          )}
+          {type === "mobile" && (
             <>
               <li className={graphicCardStyles.listItem}>
                 <IoIosCheckmarkCircleOutline
@@ -65,6 +84,9 @@ const GraphicCard = ({ type }) => {
       </div>
       {type === "web-dev" && (
         <WebDevIcon className={graphicCardStyles.svgIllustration} />
+      )}
+      {type === "mobile" && (
+        <MobileIcon className={graphicCardStyles.svgIllustration} />
       )}
       {type === "web-design" && (
         <WebDesignIcon className={graphicCardStyles.svgIllustration} />
