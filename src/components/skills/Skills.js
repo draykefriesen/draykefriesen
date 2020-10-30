096d83105ee49eff942b3922ff7b42e7cad5d90b
drayke friesen <draykefriesen@gmail.com>
2020-10-30 09:15:59 -0500

    changed background stripes in skills section

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,22 +1,22 @@
 import React from "react"
 import TechListSection from "./tech-list-section/TechListSection"
-import {BackgroundGradientTop, BackgroundGradientBottom} from '../background-gradient/BackgroundGradient'
-import {BackgroundStripes2} from '../background-stripes/BackgroundStripes'
+import {
+  BackgroundGradientTop,
+  BackgroundGradientBottom,
+} from "../background-gradient/BackgroundGradient"
+import { BackgroundStripes5 } from "../background-stripes/BackgroundStripes"
 import styles from "./skills.module.scss"
 
 const Skills = ({ skillsSectionRef }) => {
-
   return (
     <section className={styles.skillsSectionWrapper}>
-      <BackgroundGradientTop section='skills'/>
-        <h2
-          className={styles.skillsSectionHeader}
-          ref={skillsSectionRef}
-        >Skills
-        </h2>
+      <BackgroundGradientTop section="skills" />
+      <h2 className={styles.skillsSectionHeader} ref={skillsSectionRef}>
+        Skills
+      </h2>
       <TechListSection />
-      <BackgroundStripes2 />
-      <BackgroundGradientBottom section='skills' />
+      <BackgroundStripes5 />
+      <BackgroundGradientBottom section="skills" />
     </section>
   )
 }
